7c9b25a05d742bc52026f8fe51cfddaff1cf93c9
Thiago Roberto <thiago.roberto.vier4@gmail.com>
2023-08-31 21:35:21 -0300

    commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:atividade_pratica/atv_pratica.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+        home: Atv_pratica(),
+    ),
+  );
+}
